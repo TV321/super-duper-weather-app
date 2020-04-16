7f4fac1aabd207653434cb7555692d263fc7aab7
TV321 <tvlahovic7@gmail.com>
2020-04-16 22:45:07 +0200

    render data onclick

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const button = document.querySelector('button')
 let userLocation = document.querySelector('.location')
+const icon = document.querySelector('.icon img')
+const info = document.querySelector('.main-info')
 
-const apiKey = "71c4ce3c29929e4460d9506740399393"
+const key = "71c4ce3c29929e4460d9506740399393"
 
 const getData = async (lat, long) => {
-    
+    const base = `https://api.openweathermap.org/data/2.5/weather?`
+    const query = `units=metric&lat=${ lat }&lon=${ long }&appid=${ key }`
+
+    const data = await fetch(base + query)
+    const response = await data.json()
+    return response
+}
+
+const updateUI = async (data) => {
+    const html = `
+        <h3 class="card-title">${ data.name }</h5>
+
+        <h5 class="card-subtitle mb-2 text-muted">${ data.weather[0].description }</h6>
+        <h3 class="card-title">${ data.main.temp } &#8451</h3>
+    `
+    info.innerHTML = html
+    icon.setAttribute('src', `http://openweathermap.org/img/wn/${ data.weather[0].icon }@2x.png`)
 }
 
 button.addEventListener('click', () => {
     navigator.geolocation.getCurrentPosition(position => {
-        let lat = position.coords.latitude
-        let long = position.coords.longitude
+        let lat = position.coords.latitude.toFixed(2)
+        let long = position.coords.longitude.toFixed(2)
 
         
 
@@ -18,5 +36,7 @@ button.addEventListener('click', () => {
             <p>Latitude: ${ lat }</p>
             <p>Longitude: ${ long }</p>            
         `
+        getData(lat, long)
+            .then(data => updateUI(data))
     })
 })
